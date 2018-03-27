920ffa21e8a448b15b68be09af0e3518432e9dcf
Maple-lei <415024130@qq.com>
2018-03-27 22:50:07 +0800

    增加文件夹，删除无用dll

--- WebForm/Global.asax.cs
+++ WebForm/Global.asax.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-using System.Web.Optimization;
 using System.Web.Routing;
 using System.Web.Security;
 using System.Web.SessionState;
@@ -13,9 +12,7 @@ namespace WebForm
     {
         void Application_Start(object sender, EventArgs e)
         {
-            // 在应用程序启动时运行的代码
-            RouteConfig.RegisterRoutes(RouteTable.Routes);
-            BundleConfig.RegisterBundles(BundleTable.Bundles);
+         
         }
     }
 }
